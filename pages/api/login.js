7fcb26b0ab5f4d8311a054a5323603d5182bbb74
Paramvir singh <114574158+paramvirTheBlockCoders@users.noreply.github.com>
2023-11-22 18:03:20 +0530

    Update login.js

--- pages/api/login.js
+++ pages/api/login.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
       console.log(data, "to be send to api!!!!!!!!!!");
       var config = {
         method: "post",
-        url: "http://159.89.168.224:3000/api/admin/login",data
+        url: "http://16.171.110.81:3000/api/admin/login",data
       };
       await axios(config).then(function (response) {
         console.log(JSON.stringify(response.data));
