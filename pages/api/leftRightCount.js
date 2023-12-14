254b26cd586686a4ad04562cc5b75cc18c720ff2
Paramvir singh <114574158+paramvirTheBlockCoders@users.noreply.github.com>
2023-12-14 12:51:18 +0530

    Update leftRightCount.js

--- pages/api/leftRightCount.js
+++ pages/api/leftRightCount.js
@@ -2,23 +2,21 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
-  const session = await getSession({ req });
+  // const session = await getSession({ req });
   if (req.method === "POST") {
     try {
-      console.log(session.user?.name, "yyyyyyy");
+      // console.log(session.user?.name, "yyyyyyy");
 
       // Set HttpOnly cookie on the response
-      res.setHeader(
-        "Set-Cookie",
-        `token=${session.user?.name}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24 * 365}` // Example: 7 days expiry
-      );
+      // res.setHeader(
+      //   "Set-Cookie",
+      //   `token=${session.user?.name}; HttpOnly; Path=/; Max-Age=${60 * 60 * 24 * 365}` // Example: 7 days expiry
+      // );
 
       var config = {
         method: "post",
-        url: "http://44.200.180.0:7777/api/v1/admin/getuserscount",
-        headers: {
-          Authorization: `Bearer ${session.user?.name}`,
-        },
+        url: "http://44.200.180.0:7777/api/v1/auth/getuserscount",
+       
       };
 
       await axios(config).then(function (response) {
