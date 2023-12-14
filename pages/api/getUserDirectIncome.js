ad33b98e3dbb4befb5fe401b48544debdf3bf1d4
Paramvir singh <114574158+paramvirTheBlockCoders@users.noreply.github.com>
2023-12-14 12:52:03 +0530

    Update getUserDirectIncome.js

--- pages/api/getUserDirectIncome.js
+++ pages/api/getUserDirectIncome.js
@@ -2,16 +2,14 @@ import axios from "axios";
 import { getSession } from "next-auth/react";
 
 export default async function handler(req, res) {
-  const session = await getSession({ req });
+
   if (req.method === "POST") {
     try {
-      console.log(session.user?.name)
+     
       var config = {
         method: "post",
-        url: "http://44.200.180.0:7777/api/v1/admin/getallusers",
-        headers: {
-          Authorization: `Bearer ${session.user?.name} `,
-        },
+        url: "http://44.200.180.0:7777/api/v1/auth/getallusers",
+       
       };
       await axios(config).then(function (response) {
         res.status(200).json({ data: response.data.data });
